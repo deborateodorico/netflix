22fa2d61238f681cfaf834915248859f16630fa0
Débora Teodoríco <debora.steodorico@gmail.com>
2021-10-06 09:57:10 -0300

    funcionamento de item de filme numa lista

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React, { useEffect, useState } from "react";
+import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 function App() {
-    const [movieList, setMovieList] = useState([]);
-    useEffect(() => {
-        const loadAll = async () => {
-            let list = await Tmdb.getHomeList();
-            setMovieList(list);
-        }
-        loadAll();
-    },[])
-    return (
-        <div className="pages">
-            <section className="lists">
-                {movieList.map((item, key) => {
-                    return <MovieRow key={key} title={item.title} items={item.items}/>
-                })}
-            </section>
+  const [movieList, setMovieList] = useState([]);
+  const [feacturedData, setFeacturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // pegando o feactured
+      const originals = list.filter(i => i.slug === 'originals');
+      const randomChosenFilm = Math.floor(Math.random() * (originals[0].items.results.length -1)); // pegando aleatoriamente o filme
+      const chosenFilm = originals[0].items.results[randomChosenFilm]; // filme escolhido
+      const chosenInfoFilm = await Tmdb.getMovieInfo(chosenFilm.id, 'tv');
+      setFeacturedData(chosenInfoFilm);
+    }
+    loadAll();
+},[])
+
+// para adicionar um evento de monitoramento da própria pagina, ref a barra do header.
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+},[])
+
+  return (
+    <div className="pages">
+      <Header black={blackHeader}/>
+      {feacturedData && 
+        <FeaturedMovie item={feacturedData} />
+      }
+      <section className="lists">
+        {movieList.map((item, key) => {
+            return <MovieRow key={key} title={item.title} items={item.items}/>
+        })}
+      </section>
+      <footer>
+        Feito com <span role="img" aria-label="coração">♥︎</span> por Débora Teodorico<br/>
+        Direitos de imagem para Netflix <br/>
+        Dados utilizados do site Themoviedb.org
+      </footer>
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
         </div>
-    );
+      }
+    </div>
+  );
 }
 export default App;
+
+// para saber a posicao do sroll, usamos o window.scrollY, onde o Y é a vertical.
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,20 +1,54 @@
-// import react from "react";
+import react, { useState } from "react";
 import './MovieRow.css';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 function MovieRow({title, items}) {
-    console.log(items)
-    return(
-        <div>
-            <h2>{title}</h2>
-            <div className="movieRow--listarea">
-                {items.results?.length > 0 && items.results.map((item, key) => {
-                    return (
-                        <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt="posters" key={key} />
-                    );
-                })}
-            </div>
+  const [scrollX, setScrollX] = useState(0);
+
+  // para empurrar a lista para a esquerda a margem sempre tem que ser negativa.
+  const handleLeftArrow = () => {
+    let scrollValueLeft = scrollX + Math.round(window.innerWidth / 2); // para definir o numero baseado no tamanho da tela do usuário(metade da tela nesse caso)
+    if (scrollValueLeft > 0) {
+      scrollValueLeft = 0; // o scroll so vai até chegar em zero, que é o filme inicial
+    }
+    setScrollX(scrollValueLeft);
+  };
+
+  const handleRigthArrow = () => {
+    let scrollValueRigth = scrollX - Math.round(window.innerWidth / 2);
+    let listWidth = items.results.length * 200;
+    if (window.innerWidth - listWidth > scrollValueRigth) {
+      scrollValueRigth = (window.innerWidth - listWidth) - 60; // subtrai de 60 para respeitar o padding de 30px de cada lado
+    }
+    setScrollX(scrollValueRigth)
+  };
+
+  return(
+    <div className="movierow">
+      <h2>{title}</h2>
+    <div className="movierow--left" onClick={handleLeftArrow}>
+    <NavigateBeforeIcon style={{fontSize: 50}}/>
+    </div>
+    <div className="movierow--rigth" onClick={handleRigthArrow}>
+      <NavigateNextIcon style={{fontSize: 50}}/>
+    </div>
+      <div className="movierow--listarea">
+        <div className="movierow--list" style={{
+          marginLeft: scrollX,
+          width: items.results.length * 200
+        }}>
+          {items.results?.length > 0 && items.results.map((item, key) => {
+            return (
+              <div key={key} className="movierow--item">
+                <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt="posters" key={key} />
+              </div>
+              );
+            })}
         </div>
-    );
-}
+        </div>
+    </div>
+  );
+} 
 
 export default MovieRow;
